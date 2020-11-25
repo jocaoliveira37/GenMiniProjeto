d8f9a800c73d9b04d8998b8831bb5ebe49c11c84
Pamela Souza <pamelacsouza1990@hotmail.com>
2020-11-24 21:52:01 -0300

    Add grupo 3

---
+++ src/Projeto/Grupo3.java
@@ -0,0 +1,21 @@
+package Projeto;
+
+public class Grupo3 extends Pessoas {
+
+	public Grupo3(int idade, boolean comorb, boolean gravPuer) {
+		super(idade, comorb, gravPuer);
+
+	}
+
+	public void dataVacinacao(int idade) {
+		if (idade >= 5 && idade <= 40) {
+			System.out.println("Seu mês de vacinação é Junho de 2021");
+		} else {
+
+			System.out.println("Entre em contato com a Secretaria de Saúde de sua cidade.");
+
+		}
+
+	}
+
+}
